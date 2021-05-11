217a2d12f00ced1d827855271f518c1033f1a1a3
RORLe <RORLe@DESKTOP-QL5DQK0>
2021-05-11 15:10:31 -0500

    Calculadora

---
+++ src/calculadora/pkg60180/pkg11/mayo/Calculadora6018011Mayo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora.pkg60180.pkg11.mayo;
+
+/**
+ *
+ * @author RORLe
+ */
+public class Calculadora6018011Mayo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Calculadora calculadora = new Calculadora();
+
+        calculadora.suma(25,8,21);
+        calculadora.resta(8);
+        calculadora.multiplicacion(2);
+        calculadora.suma(2,31);
+        calculadora.eliminar();
+        calculadora.resultado();
+    }   
+}
